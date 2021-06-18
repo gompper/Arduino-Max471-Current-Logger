c9a60b543ebc5632aa64bad48b1ce0495a2d35ff
Urs <urs.gompper@posteo.com>
2021-06-18 08:13:38 +0200

    add: boilerplate code

--- py/serial_read.py
+++ py/serial_read.py
@@ -17,40 +17,43 @@ def saveDataToFile(data):
     np.save(f,data)
     f.close()
 
-buffer = ""
-buf_arr = []
-with serial.Serial('COM7', 115200, timeout=1) as ser:
-    start = time.time()
-    while(len(buf_arr)<10000):
-        # read one byte
-        oneByte = ser.read(1)
-        
-        # test if received byte is line break
-        if oneByte == b"\r":    
-            try:
-                buf_int = int(buffer)
-                pass
-            except ValueError:
-                buffer = ""
-                continue
+def capture():
+    buffer = ""
+    buf_arr = []
+    with serial.Serial('COM6', 115200, timeout=1) as ser:
+        start = time.time()
+        while(len(buf_arr)<1000):
+            # read one byte
+            oneByte = ser.read(1)
             
-            # convert raw data to ampère
-            buf_converted = ADCRaw2Amp(buf_int,10,5)
-            buf_arr.append(buf_converted)
-            buffer = ""
-        else:
-            try:
-                buffer += oneByte.decode("utf-8") 
-            except UnicodeDecodeError:
+            # test if received byte is line break
+            if oneByte == b"\r":    
+                try:
+                    buf_int = int(buffer)
+                    pass
+                except ValueError:
+                    buffer = ""
+                    continue
+                
+                # convert raw data to ampère
+                buf_converted = ADCRaw2Amp(buf_int,10,5)
+                buf_arr.append(buf_converted)
                 buffer = ""
-                continue
-    end = time.time()
-    
-    # calculate samples/second (averaging over 64 samples is done on arduino)
-    SamplesPerSecond = (len(buf_arr)/(end-start))*64
-    print(round(SamplesPerSecond,2), "sample/s")
-
-    buf_np = np.asarray(buf_arr)
-    saveDataToFile(buf_np)
-    plot_amps.plotData(FILE)
+            else:
+                try:
+                    buffer += oneByte.decode("utf-8") 
+                except UnicodeDecodeError:
+                    buffer = ""
+                    continue
+        end = time.time()
         
+        # calculate samples/second (averaging over 64 samples is done on arduino)
+        SamplesPerSecond = (len(buf_arr)/(end-start))*64
+        print(round(SamplesPerSecond,2), "sample/s")
+
+        buf_np = np.asarray(buf_arr)
+        saveDataToFile(buf_np)
+        plot_amps.plotData(FILE)
+        
+if __name__ == "__main__":
+    capture()
