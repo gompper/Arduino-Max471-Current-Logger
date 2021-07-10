0679cb279a7906db6de7c800c6645d1a1d1ec776
Urs <urs.gompper@posteo.com>
2021-07-10 12:20:56 +0200

    ADD: start measure on interrupt pin and stop measure with counter

--- arduino/read_analog/read_analog.ino
+++ arduino/read_analog/read_analog.ino
@@ -1,18 +1,55 @@
 // #define CONTINUOUS_SAMPLE
 
-const unsigned int numReadings = 12;
+const unsigned int numReadings = 1;
 const int analogPin = A3;
 const int digitalPin = 2;
-const int led = 13;
+bool stopMeasure = true;
 unsigned int analogVals[numReadings];
+
+void setupTimer1() {
+  // Deactivate Interrupts
+  noInterrupts();
+  // Normal Port Operation
+  TCCR1A = 0; 
+  // stop timer
+  TCCR1B = 0; 
+  // set timer counter
+  TCNT1 = 0;  
+  // output compare // 200 ms = 5 Hz
+  OCR1A = 5*1563; 
+  // Output Compare Match A Interrupt Enable
+  TIMSK1 |= (1 << OCIE1A);
+  // activate interrupts
+  interrupts();
+}
+
+void startTimer(){
+  stopMeasure = false;
+  // CTC (clear timer on compare)
+  TCCR1B |= (1 << WGM12);
+  // Prescaler 1024
+  TCCR1B |= (1 << CS12) | (1 << CS10);
+}
+
+void stopTimer(){
+  stopMeasure = true;
+  //stop timer
+  TCCR1B = 0; 
+  //clear timer counts
+  TCNT1 = 0;  
+}
 
 void setup()
 {
+  // connect interrupt service routine
+  attachInterrupt (digitalPinToInterrupt (digitalPin), startTimer, RISING);  // attach interrupt handler
   Serial.begin(115200);
-  ADCSRA |= (1 << ADPS2); // 16 Prescaler
+  // set ADC prescaler to 16
+  ADCSRA |= (1 << ADPS2); 
   pinMode(digitalPin,INPUT);
   pinMode(led,OUTPUT);
   pinMode(analogPin,INPUT);
+  setupTimer1();
 }
 
 void measureAndSend(){
@@ -29,11 +66,14 @@ void loop()
   #ifdef CONTINUOUS_SAMPLE
     measureAndSend();
   #else
-    if (digitalRead(digitalPin)){
-      measureAndSend();
-      digitalWrite(led, HIGH);
+    if (digitalRead(digitalPin) && stopMeasure){
+      startTimer();
     }else{
-      digitalWrite(led, LOW);
+      while (!stopMeasure) measureAndSend();
     }
   #endif
+}
+
+ISR(TIMER1_COMPA_vect) {
+  stopTimer();
 }
