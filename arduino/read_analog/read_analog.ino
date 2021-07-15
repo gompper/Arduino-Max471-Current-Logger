a0d0e4688af7ceb1b1cd884d9db23c63a7966395
Urs <urs.gompper@posteo.com>
2021-07-15 12:09:29 +0200

    FIX: stop with timer and falling edge works now

--- arduino/read_analog/read_analog.ino
+++ arduino/read_analog/read_analog.ino
@@ -1,14 +1,25 @@
+/*
+* Uncomment one of the following three
+*/
 // #define CONTINUOUS_SAMPLE
-// #define START_SAMPLE_WITH_FALLING_EDGE // else with timer interrupt
+#define STOP_SAMPLE_WITH_FALLING_EDGE 
+// #define STOP_SAMPLE_WITH_TIMER
 
-const unsigned int numReadings = 1;
-const int analogPin = A3;
 const int digitalPin = 2;
-bool stopMeasure = true;
-unsigned int analogVals[numReadings];
-int frameCounter = 0;
-int frameCounter_old = 0;
+const int ledPin = 13;
+const int analogPin = A3;
 
+volatile int measure = 0;
+
+typedef enum{
+  IDLE,
+  MEASURE
+} State;
+
+State state = IDLE;
+State nextState = IDLE;
+
+#ifdef STOP_SAMPLE_WITH_TIMER
 void setupTimer1() {
   // Deactivate Interrupts
   noInterrupts();
@@ -26,66 +37,74 @@ void setupTimer1() {
   interrupts();
 }
 
-void startTimer(){
-  frameCounter++;
-  stopMeasure = false;
+ISR(TIMER1_COMPA_vect) {
+  //stop timer
+  TCCR1B = 0; 
+  //clear timer counts
+  TCNT1 = 0;
+
+  nextState = IDLE;
+}
+#endif
+
+void rising(){
+  nextState = MEASURE;
+  #ifdef STOP_SAMPLE_WITH_TIMER
   // CTC (clear timer on compare)
   TCCR1B |= (1 << WGM12);
   // Prescaler 1024
   TCCR1B |= (1 << CS12) | (1 << CS10);
+  #endif
+}
+void falling(){
+  #ifdef STOP_SAMPLE_WITH_FALLING_EDGE
+  nextState = IDLE;
+  #endif
 }
 
-void stopTimer(){
-  stopMeasure = true;
-  //stop timer
-  TCCR1B = 0; 
-  //clear timer counts
-  TCNT1 = 0;  
+void trigger(){
+  if(digitalRead(digitalPin)) rising();
+  else falling();
 }
 
 void setup()
 {
-  // connect interrupt service routine
-  attachInterrupt (digitalPinToInterrupt (digitalPin), startTimer, RISING);  // attach interrupt handler
-  #ifdef START_SAMPLE_WITH_FALLING_EDGE
-    attachInterrupt (digitalPinToInterrupt (digitalPin), stopTimer, FALLING);  // attach interrupt handler
-  #endif
-  Serial.begin(115200);
+  attachInterrupt (digitalPinToInterrupt (digitalPin), trigger, CHANGE);  // attach interrupt handler
+
   // set ADC prescaler to 16
   ADCSRA |= (1 << ADPS2); 
-  pinMode(digitalPin,INPUT);
-  pinMode(analogPin,INPUT);
-  setupTimer1();
-}
 
-void measureAndSend(){
-  for (int i=0; i< numReadings;i++){
-    analogVals[i] = analogRead(analogPin);
-  }
-  if (frameCounter != frameCounter_old){
-    Serial.println("S");
-    frameCounter_old = frameCounter;
-  }
-  for (int i=0; i< numReadings;i++){
-    Serial.println(analogVals[i]);
-  }
+  Serial.begin(115200);
+
+  pinMode(digitalPin,INPUT);
+  pinMode(ledPin,OUTPUT);
+  pinMode(analogPin,INPUT);
+
+  Serial.println("Start");
+  
+  #ifdef STOP_SAMPLE_WITH_TIMER
+  setupTimer1();
+  #endif
 }
 
 void loop()
 {
-  #ifdef CONTINUOUS_SAMPLE
-    measureAndSend();
-  #else
-    if (digitalRead(digitalPin) && stopMeasure){
-      startTimer();
-    }else{
-      while (!stopMeasure) measureAndSend();
-    }
-  #endif
-}
-
-ISR(TIMER1_COMPA_vect) {
-#ifndef START_SAMPLE_WITH_FALLING_EDGE
-  stopTimer();
-#endif
+  switch (state)
+  {
+    case IDLE:
+      if (nextState != state) {
+        state = nextState;
+        Serial.println("B");
+      }
+      break;
+    case MEASURE:
+      Serial.println(analogRead(analogPin));
+      if (nextState != state) {
+        state = nextState;
+        Serial.println("E");
+      }
+      break;
+    default:
+      break;
+  }
 }
